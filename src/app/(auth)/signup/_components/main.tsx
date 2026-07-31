de1b84116f56c14682b6dfa7ed1821f762ef68dd
loohhostels-lab <loohhostels@gmail.com>
2026-07-31 17:16:14 +0530

    signup is done

--- src/app/(auth)/signup/_components/main.tsx
+++ src/app/(auth)/signup/_components/main.tsx
@@ -5,18 +5,22 @@ import { z } from "zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button"
 import { SignupOtherInput } from "./others-input";
+import { signUp } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 // type of signup
 const signupSchema = z.object({
   name: z.string().min(1, { error: "enter your name" }),
   email: z.email({ message: "enter valid email" }).regex(/^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/, { message: "enter valid email" }),
-  password: z.string().min(1, { error: "password should be at least 8 character long" })
+  password: z.string().min(8, { error: "password should be at least 8 character long" })
 })
 
 // infer type from signupSchema
 export type SignupSchema = z.infer<typeof signupSchema>
 
 export function SignupForm() {
+  const router = useRouter();
   // initalization react hook form with type
   const form = useForm<SignupSchema>({
     resolver: zodResolver(signupSchema),
@@ -27,8 +31,23 @@ export function SignupForm() {
     },
   });
 
-  function onSubmit(data: SignupSchema) {
-    console.log(data)
+  async function onSubmit(SignupData: SignupSchema) {
+    // on submit, do the signup
+    const { data, error } = await signUp.email({
+      email: SignupData.email,
+      password: SignupData.password,
+      name: SignupData.name,
+    }, {
+      onSuccess: () => {
+        toast.success("signup successful");
+        router.push("/signin");
+      },
+      onError: (ctx) => {
+        form.setError("root", {
+          message: ctx.error.message
+        })
+      }
+    });
   }
 
   return (
@@ -51,6 +70,9 @@ export function SignupForm() {
         )}
       />
       <SignupOtherInput form={form} />
+      {form.formState.errors.root && (
+        <p className="text-red-500 text-sm font-medium my-2" role="alert">{form.formState.errors.root.message}</p>
+      )}
       <Button type="submit">Create Account</Button>
     </form>
   )
---
+++ src/app/(auth)/signup/_components/toast-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Toaster } from "sonner";
+
+export function CustomeToast() {
+    return (
+        <Toaster
+            closeButton
+            position="top-right"
+            toastOptions={{
+                classNames: {
+                    toast: "!font-mono !font-bold !border !shadow-lg !rounded-xl",
+                    success: "!bg-emerald-600 !text-white !border-emerald-600",
+                    title: "!font-bold",
+                    error: "!bg-rose-600 !text-white !border-rose-600",
+                    info: "!bg-sky-600 !text-white !border-sky-600",
+                    warning: "!bg-amber-600 !text-white !border-amber-600",
+                },
+            }}
+        />
+    );
+}
